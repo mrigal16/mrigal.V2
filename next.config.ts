a8d835e062e7651ba0b41b4063175bd93510163d
mrigal16 <mrigal.digitservz16@gmail.com>
2025-07-10 00:34:30 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
+    
+  },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://mrigal.digitservz.dz/:path*",
+        permanent: true, // 308 status for permanent redirect
+      },
+    ];
   },
 };
 
